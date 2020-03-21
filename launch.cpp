98a7a2779443e813541777be1fff2c234cd8a1d2
deinBestFreund <62200071+deinBestFreund@users.noreply.github.com>
2020-03-21 06:54:07 -0400

    someone added two mains reverting changes

--- launch.cpp
+++ launch.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h> 
-using namespace std;
-
-
-string getPath(int numArgs, char *passedArgs[])
-{
-    // Get the last position of '/'
-    string aux(passedArgs[0]);
-    // get '/' or '\\' depending on unix/mac or windows.
-#if defined(_WIN32) || defined(WIN32)
-    int pos = aux.rfind('\\');
-#else
-    int pos = aux.rfind('/');
-#endif
-    // Get the path and the name
-    string path = aux.substr(0,pos+1);
-    string name = aux.substr(pos+1);
-    // show results
-    return (path);
-    //cout << "Name: " << name << std::endl;
-
-}
-
-bool isUpdated;
-
-string getPlayerID()
-{ 
-// 获取并返回可以识别玩家身份的id
-return "encodedString";
-}
-
-const string playerID=getPlayerID(); 
-
-int update()
-{#include <iostream>
-#include <string>
-#include <bits/stdc++.h> 
 #include <cstdio>
 #include <memory>
 #include <stdexcept>
@@ -117,31 +81,6 @@ int main(int numArgs, char *args[])
 		string updater=busyboxPath+" wget -P "+getPath(numArgs, args)+" http://files.ultirts.net/newrelease/updater && "+getPath(numArgs, args)+"updater";
 		system(updater.c_str()); //get the newest version and avoid writing to the actual disk if possible
 		cout << "Updated, running new launcher"<<endl;
-		system(launcherPath.c_str());		
-		return 0;		
-		}
-	else 
-		{
-		cout << "launcher path is "<< getPath(numArgs, args)<<endl;
-		
-		cout << "infering engine path "<< enginePath<<endl;
-		system(enginePath.c_str());
-		}
-    	return 0;
-}
-// get id 到服务器并下载更新
-}
-
-int main(int numArgs, char *args[]) 
-{
-	string launcherPath=getPath(numArgs, args)+"launcher.exe";
-	string enginePath=getPath(numArgs, args)+"engine/spring --menu 'Chobby $VERSION'";
-	cout << "Checking for update"<<endl;
-	
-	if (isUpdated)
-		{
-		update();
-
 		system(launcherPath.c_str());		
 		return 0;		
 		}
